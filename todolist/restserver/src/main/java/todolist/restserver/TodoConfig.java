2ebf4a21fcc6b59b4e936496a020af1e4ca5d763
Hallvard Trætteberg <hal@ntnu.no>
2020-10-06 13:05:33 +0000

    Rigget opp rest-api og -server (#14)

--- todolist/restserver/src/main/java/todolist/restserver/TodoConfig.java
+++ todolist/restserver/src/main/java/todolist/restserver/TodoConfig.java
@@ -10,28 +10,39 @@ import todolist.restapi.TodoModelService;
 
 public class TodoConfig extends ResourceConfig {
 
+  private TodoModel todoModel;
+
   /**
    * Initialize this TodoConfig.
    *
    * @param todoModel todoModel instance to serve
    */
   public TodoConfig(TodoModel todoModel) {
+    setTodoModel(todoModel);
     register(TodoModelService.class);
     register(TodoModuleObjectMapperProvider.class);
     register(JacksonFeature.class);
     register(new AbstractBinder() {
       @Override
       protected void configure() {
-        bind(todoModel);
+        bind(TodoConfig.this.todoModel);
       }
     });
   }
-
+  
   /**
    * Initialize this TodoConfig with a default TodoModel.
    */
   public TodoConfig() {
     this(createDefaultTodoModel());
+  }
+
+  public TodoModel getTodoModel() {
+    return todoModel;
+  }
+  
+  public void setTodoModel(TodoModel todoModel) {
+    this.todoModel = todoModel;
   }
 
   private static TodoModel createDefaultTodoModel() {
