1def2e00ee4e935815dd1e665d4c6212f305312f
Hallvard Trætteberg <hal@ntnu.no>
2021-09-29 11:48:28 +0000

    Trying to make springboot work with modules

--- todolist/springboot/restserver/src/main/java/module-info.java
+++ todolist/springboot/restserver/src/main/java/module-info.java
@@ -2,7 +2,9 @@ module todolist.springboot.rest {
     requires com.fasterxml.jackson.databind;
 
     requires spring.web;
+    requires spring.beans;
     requires spring.boot;
+    requires spring.boot.autoconfigure;
 
     requires todolist.core;
 }
