24a71ebaff98368709c732ad34647f213c1ded12
Adrian Stoica <stoica@ntnu.no>
2022-11-03 17:03:46 +0100

    fix: corrects list rename uri

--- todolist/fxui/src/main/java/todolist/ui/RemoteTodoModelAccess.java
+++ todolist/fxui/src/main/java/todolist/ui/RemoteTodoModelAccess.java
@@ -226,7 +226,7 @@ public class RemoteTodoModelAccess implements TodoModelAccess {
   @Override
   public void renameTodoList(String oldName, String newName) {
     try {
-      HttpRequest request = HttpRequest.newBuilder(todoListUri(oldName))
+      HttpRequest request = HttpRequest.newBuilder(todoListUri(oldName).resolve(uriParam(oldName)+"/rename"))
           .header(ACCEPT_HEADER, APPLICATION_JSON)
           .header(CONTENT_TYPE_HEADER, APPLICATION_FORM_URLENCODED)
           .POST(BodyPublishers.ofString("newName=" + uriParam(newName)))
