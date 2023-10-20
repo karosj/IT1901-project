ba0eb773811015fc8205fd78acf92766ef107cce
Emil Moberg Vaernes <vaernes@emilmoberg.com>
2023-10-20 16:25:27 +0200

    style: switch card content and time header

--- schedulelog/frontend/src/components/ActivityCard/ActivityCard.tsx
+++ schedulelog/frontend/src/components/ActivityCard/ActivityCard.tsx
@@ -4,7 +4,7 @@ import styles from "./ActivityCard.module.css"
 export default function ActivityCard({ content, startTime }: Activity) {
 
     return <div className={styles.card}>
-        <h3>{content}</h3>
-        <p>{new Date(startTime).toLocaleString()}</p>
+        <h3>{new Date(startTime).toLocaleString()}</h3>
+        <p>{content}</p>
     </div>
 }
