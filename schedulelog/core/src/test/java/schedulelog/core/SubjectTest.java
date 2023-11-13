e2577f9818e6abdc118fb1a33b116e4be1af29bf
Marcus Larsen-Frivoll <marcula@stud.ntnu.no>
2023-11-13 21:32:08 +0100

    Added descriptions to SubjectTests

--- schedulelog/core/src/test/java/schedulelog/core/SubjectTest.java
+++ schedulelog/core/src/test/java/schedulelog/core/SubjectTest.java
@@ -22,35 +22,35 @@ public class SubjectTest {
     @Test
     public void testSubjectConstructionWithValidCode() {
         Subject subject = new Subject("TDT4140", courses);
-        assertEquals("TDT4140", subject.getCode());
-        assertEquals("Programvareutvikling", subject.getName());
+        assertEquals("TDT4140", subject.getCode(),"Subject code should be 'TDT4140' when constructed with a valid code.");
+        assertEquals("Programvareutvikling", subject.getName(),"Subject name should be 'Programvareutvikling' when constructed with a valid code 'TDT4140'.");
     }
 
     @Test
     public void testSubjectConstructionWithInvalidCode() {
-        assertThrows(IllegalArgumentException.class, () -> new Subject("TDT9999", courses));
+        assertThrows(IllegalArgumentException.class, () -> new Subject("TDT9999", courses),"Constructing a subject with an invalid code should throw an IllegalArgumentException.");
     }
 
     @Test
     public void testConstructorWithNullCode() {
-        assertThrows(IllegalArgumentException.class, () -> new Subject(null, courses));
+        assertThrows(IllegalArgumentException.class, () -> new Subject(null, courses),"Constructing a subject with a null code should throw an IllegalArgumentException.");
     }
 
     @Test
     public void testConstructorWithEmptyCode() {
-        assertThrows(IllegalArgumentException.class, () -> new Subject("", courses));
+        assertThrows(IllegalArgumentException.class, () -> new Subject("", courses),"Constructing a subject with an empty code should throw an IllegalArgumentException.");
     }
 
     @Test
     public void testConstructorWithNullCourses() {
-        assertThrows(IllegalArgumentException.class, () -> new Subject("TDT4140", null));
+        assertThrows(IllegalArgumentException.class, () -> new Subject("TDT4140", null),"Constructing a subject with null courses should throw an IllegalArgumentException.");
     }
 
     @Test
     public void testToString() {
         Subject subject = new Subject("TDT4140", courses);
         String representation = subject.toString();
-        assertTrue(representation.contains("TDT4140"));
-        assertTrue(representation.contains("Programvareutvikling"));
+        assertTrue(representation.contains("TDT4140"),"Subject's string representation should contain the code 'TDT4140'.");
+        assertTrue(representation.contains("Programvareutvikling"),"Subject's string representation should contain the name 'Programvareutvikling'.");
     }
 }
